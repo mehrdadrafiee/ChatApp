657924c6c676bcf1520079e1201d240f44d2a8e5
mrafiee <rafiee.mehrdad@gmail.com>
2016-09-24 14:08:54 -0700

    adds form validation to login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
+let { computed } = Ember;
+
 export default Ember.Controller.extend({
   emailAddress: '',
-  
-  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid')
+
+  isValid: computed.match('emailAddress', /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i),
+  isDisabled: computed.not('isValid')
 });
