f36977d3d128ba19b1355a5096cd0a8ba777b7bb
mrafiee <rafiee.mehrdad@gmail.com>
2016-09-24 18:13:32 -0700

    adds more validation to the login form

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,7 +4,9 @@ let { computed } = Ember;
 
 export default Ember.Controller.extend({
   emailAddress: '',
+  password: '',
 
-  isValid: computed.match('emailAddress', /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i),
-  isDisabled: computed.not('isValid')
+  isValidEmail: computed.match('emailAddress', /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i),
+  isValidPassword: computed.gte('password.length', 5),
+  isValid: computed.and('isValidEmail', 'isValidPassword')
 });
