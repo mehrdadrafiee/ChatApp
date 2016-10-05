76ffc895dfe58b73e2a56dfc5c9d86438eaf83b9
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-10-04 23:12:39 -0700

    Used promise to make sure that username is saved

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,16 +4,23 @@ let { computed } = Ember;
 
 export default Ember.Controller.extend({
   username: '',
+  id: '',
 
   validUsername: computed.gte('username.length', 5),
   actions: {
     saveUsername() {
       const username = this.get('username');
-      const newUser = this.store.createRecord('users', { username: username });
-      newUser.save();
 
-      this.set('greetMessage', `Welcome ${this.get('username')}! Hang on while we are preparing the chatroom.`);
-      this.set('username', '');
+      const newUser = this.store.createRecord('users', {
+        username: username
+      });
+
+      newUser.save().then((response) => {
+        this.set('greetMessage', `Welcome ${this.get('username')}! Hang on while we are preparing the chatroom.`);
+        this.set('username', '');
+        this.set('id', response.get('id'));
+        this.transitionToRoute('index');
+      });
     }
   }
 });
