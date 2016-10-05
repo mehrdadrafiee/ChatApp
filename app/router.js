5823b84745d3011f33bd6ffacae62b065fef799c
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-10-04 21:16:24 -0700

    init commit

--- app/router.js
+++ app/router.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('login');
   this.route('register');
+  this.route('login');
 });
 
 export default Router;
